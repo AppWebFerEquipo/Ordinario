bf163ba9021efc36a54eea746f5ae3b443994862
AppWebFerEquipo <91996493+AppWebFerEquipo@users.noreply.github.com>
2022-04-05 19:01:21 -0500

    Add files via upload

---
+++ PeliculaAPI/Controllers/PelisController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PeliculaAPI.Models;
+using PeliculaAPI.Repositories;
+
+namespace PeliculaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PelisController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            RPPelis rpPelis = new RPPelis();
+            return Ok(rpPelis.ObtenerPeliculas());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            RPPelis rpPelis = new RPPelis();
+
+            var cliRet = rpPelis.ObtenerPeliculas(id);
+
+            if (cliRet == null)
+            {
+                var nf = NotFound("La pelicula " + id.ToString() + " no existe.");
+                return nf;
+            }
+
+            return Ok(cliRet);
+        }
+
+        [HttpPost("agregar")]
+        public IActionResult AgregarPelicula(Peliculas nuevaPeliculas)
+        {
+            RPPelis rpPelis = new RPPelis();
+            rpPelis.Agregar(nuevaPeliculas);
+            return CreatedAtAction(nameof(AgregarPelicula), nuevaPeliculas);
+        }
+    }
+}
+
+
+
---
+++ PeliculaAPI/Models/Peliculas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculaAPI.Models
+{
+    public class Peliculas
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Director { get; set; }
+        public string Genero { get; set; }
+        public string Puntuacion { get; set; }
+        public string Rating { get; set; }
+        public string AnioPub { get; set; }
+    }
+}
